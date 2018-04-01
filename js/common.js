07c8cff028e9da946907e8a368ea04a7164183bb
Aleksandr-Tarkovskiy <forestera.net@mail.ru>
2018-04-01 15:00:25 +0300

    finished

--- js/common.js
+++ js/common.js
@@ -1,20 +1,41 @@
 $(function() {
 
 	$('.owl-carousel').owlCarousel({
-    loop:true,
-    margin:10,
-    nav:true,
-    responsive:{
-        0:{
-            items:1
-        },
-        600:{
-            items:3
-        },
-        1000:{
-            items:5
-        }
-    }
+		loop: true,
+		margin: 15,
+		nav: true,
+		dots: false,
+		responsiveClass:true,
+		responsive:{
+				1:{
+						items:2,
+						nav:false
+				},
+				576:{
+						items:3,
+						nav:false
+				},
+				768:{
+						items:4,
+						nav:false
+				},
+				1024:{
+						items:4,
+						nav:true
+				}
+		}
 })
+
+$(document).ready(function() {
+	$('.i-click-1').on('click', function() {
+		$('.slide-1').slideToggle(250);
+		$('.up-1').toggle();
+	});
+	$('.i-click-2').on('click', function() {
+		$('.slide-2').slideToggle(250);
+		$('.up-2').toggle();
+	});
+});
+
 
 });
